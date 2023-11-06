cd011304d9c11525330e317cef607e4f28455296
Maxishoo <92300834+Maxishoo@users.noreply.github.com>
2023-11-06 21:48:27 +0300

    Update mn.c

--- Laba5/Laba5ci/Laba5ci/mn.c
+++ Laba5/Laba5ci/Laba5ci/mn.c
@@ -1,7 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-typedef struct circle circle;
-typedef struct dot dot;
 struct dot
 {
 	int x;
@@ -35,19 +33,14 @@ int max(int a, int b)
 		return b;
 	}
 }
+typedef struct circle circle;
+typedef struct dot dot;
 int sign(int a)
 {
 	if (a >= 0)
 		return 1;
 	else
-		return 0;
-}
-int check_circle(circle c, dot p)
-{
-	if ((p.x - c.x0) * (p.x - c.x0) + (p.y - c.y0) * (p.y - c.y0 <= c.r * c.r))
-		return 1;
-	else
-		return 0;
+		return -1;
 }
 int main()
 {
@@ -56,11 +49,11 @@ int main()
 	int l = -3;
 	struct circle vnutr = { 10, 10, 5 };
 	struct circle vnesh = { 10, 10, 10 };
-
-	for (int i = 1; i <= 50; i++)
+	int fl = 0;
+	for (int i = 0; i < 50; i++)
 	{
-		point.x = (min(old_point.x + old_point.y, old_point.x + l) * (i + l)) % 30;
-		point.x = old_point.y + (l * sign(old_point.y)) % 20 + (i * sign(old_point.x)) % 10;
+		point.x = (min(old_point.x + old_point.y, old_point.x + l) * (i + 1)) % 30;
+		point.y = old_point.y + (l * sign(old_point.y)) % 20 + (i * sign(old_point.x)) % 10;
 		l = (max(max(old_point.x * old_point.y, old_point.x * l), old_point.y * l)) % 30;
 		printf("x: %d y: %d\n", point.x, point.y);
 		if ((point.x - vnesh.x0) * (point.x - vnesh.x0) + (point.y - vnesh.y0) * (point.y - vnesh.y0) <= vnesh.r * vnesh.r)
@@ -68,10 +61,13 @@ int main()
 			if ((point.x - vnutr.x0) * (point.x - vnutr.x0) + (point.y - vnutr.y0) * (point.y - vnutr.y0) >= vnutr.r * vnutr.r)
 			{
 				printf("Succes: x: %d y: %d\n", point.x, point.y);
+				fl = 1;
 				break;
 			}
 		}
 		old_point.x = point.x;
 		old_point.y = point.y;
 	}
+	if (fl == 0)
+		printf("No in ring");
 }
